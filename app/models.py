34d754a1a8f5b6ea1e93244d4150f5468f016849
Unknown <benjamin.hurtado@ing.uchile.cl>
2018-05-02 19:25:20 -0300

    Added requirements y models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Sala(models.Model):
+    pass
+
+class Articulo(models.Model):
+    nombre = 
+
+class Usuario():
+    pass
+
+class Reserva(models.Model):
+    pass
+
