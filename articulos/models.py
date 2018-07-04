44d1433c6a6aa6c0a9f65922ef24d982f8123a48
T Ismael Verdugo <tomas.verdugo@ug.uchile.cl>
2018-07-04 15:15:24 -0400

    Modelos Artículos y Reservas extendidos
    se cambió el campo 'estado' de artículos para que solo tome valores que corresponden a los estados aceptados.
    Se agrego a las reservas el campo 'autorizador', que relaciona a un usuario administrador que es el que permite el prestamo, con el prestamo.

--- articulos/models.py
+++ articulos/models.py
@@ -5,14 +5,32 @@ class Articulo(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(max_length=1000)
     foto = models.ImageField(upload_to='InventarioCEI/static/images', blank=True, null=True)
-    estadoActual = models.BooleanField(default=True)
+
+    DISPONIBLE = 'DIS'
+    ENPRESTAMO = 'PRS'
+    ENREPARACION = 'REP'
+    PERDIDO = 'PER'
+    ESTADO = (
+        (DISPONIBLE, 'Disponible'),
+        (ENPRESTAMO, 'En Prestamo'),
+        (ENREPARACION, 'En Reparación'),
+        (PERDIDO, 'Perdido'),
+    )
+    estado = models.CharField(
+        max_length=3,
+        choices=ESTADO,
+        default=DISPONIBLE,
+    )
 
     def __str__(self):
-        return self.nombre
+        return self.nombre + str(self.id)
 
 
 class ReservaArticulo(models.Model):
     articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    autorizador = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True,
+        on_delete=models.DO_NOTHING, limit_choices_to={'is_admin': True},
+        related_name='maps')
     entrega = models.DateTimeField()
     devolucion = models.DateTimeField()
