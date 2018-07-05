f7ddaafb987d4d553926dae2ecf86c6bce3ca049
Benjamín Hurtado Torrealba <benjamin.hurtado@ing.uchile.cl>
2018-07-05 12:17:59 -0400

    Primera base navbar y configuraciones de bootstrap

--- InventarioCEI/settings.py
+++ InventarioCEI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 	'autenticacion.apps.AutenticacionConfig',
     'salas.apps.SalasConfig',
     'articulos.apps.ArticulosConfig',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'InventarioCEI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
