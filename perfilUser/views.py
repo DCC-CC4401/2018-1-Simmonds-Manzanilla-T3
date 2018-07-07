38348208825fee9b7a55d72712837c759e1843cc
T Ismael Verdugo <tomas.verdugo@ug.uchile.cl>
2018-07-07 00:09:36 -0400

    Nueva base de datos de juguete
    esta todo descrito en el archivo poblarBaseDeDatos ahora se puede romper, se corren esos comandos en la shell de python manage.py, y se generan varias cosillas, la idea es tener una base constante para así poder romper la base de datos al antojo.

--- perfilUser/views.py
+++ perfilUser/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from espacios.models import ReservaEspacio
 from articulos.models import ReservaArticulo
+
+
 @login_required
 def informacionUser(request):
+    """ Vista de la pantalla de perfil de usuario, contiene la navbar,
+        botones que redireccionan hacia la reserva de salas y
+        a la reserva de artículos, y listados (ordenados por fecha)
+        de los últimos 10 espacios reservados, así como los últimos 10
+        artículos reservados, cada uno conteniendo el nombre del
+        espacio (o artículo) y su descripcion."""
     usuario_actual = request.user
-    ultimos_articulos_reservados = ReservaArticulo.objects.filter(usuario=usuario_actual).order_by('-devolucion')[:10]
-    ultimos_espacios_reservados = ReservaEspacio.objects.filter(usuario=usuario_actual).order_by('-fin')[:10]
+    ultimos_articulos_reservados = ReservaArticulo.objects.filter(
+        usuario=usuario_actual).order_by('-devolucion')[:10]
+    ultimos_espacios_reservados = ReservaEspacio.objects.filter(
+        usuario=usuario_actual).order_by('-fin')[:10]
     context = {
         'usuario' : usuario_actual,
         'ultimos_articulos_reservados' : ultimos_articulos_reservados,
