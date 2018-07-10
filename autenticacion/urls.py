41979d1a8263bf852f01368c0a0e20f40cd67ea9
T Ismael Verdugo <tomas.verdugo@ug.uchile.cl>
2018-07-10 00:42:34 -0400

    Unidas las paginas por el menu lateral
    horariios con variables generales

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('newuser', views.newuser, name='newuser'),
 	path('login_view', views.login_view, name='login_view'),
-	path('login_succes', views.login_succes, name='login_succes'),
 	path('logout_view', views.logout_view, name='logout_view'),
-	path('logout_succes', views.logout_succes, name='logout_succes'),
 ]
